b44986ad950cbd9cc0cc8487d220fb2063f5b435
Yohavier <Jonas@Knohl.de>
2022-03-16 20:58:32 +0100

    create todo list

--- Wwizard/Wwizard/main.cpp
+++ Wwizard/Wwizard/main.cpp
@@ -1,5 +1,14 @@
 #include "Application.h"    
 
+/*TODO:
+* - opening wwise, connecting app, close wwise -> app fails to reconnect
+* - create connection icon (green/red)
+* - reconnect button
+* - reflect color coding from wwise in query results
+* - if wwise query create inspect button in details and open in wwise app if it is open
+* - implement sort originals 
+*/
+
 int main()
 {
     std::unique_ptr<Application> app(new Application());
