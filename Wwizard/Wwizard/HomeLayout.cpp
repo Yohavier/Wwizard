a9c5e80b4394afabcca11b103af7b5398274c123
Yohavier <Jonas@Knohl.de>
2022-05-12 00:08:18 +0200

    remove roadmap text

--- Wwizard/Wwizard/HomeLayout.cpp
+++ Wwizard/Wwizard/HomeLayout.cpp
@@ -13,33 +13,5 @@ void HomeLayout::RenderLayout()
         ImGui::End();
         return;
     }
-    ImGui::Text("Roadmap");
-    ImGui::Separator();
-    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(ConvertWwiseColorToRGB(5)));
-    ImGui::Text("---- Release Version 1.0 ----");
-    ImGui::Indent(15);
-    ImGui::Text("First stable version");
-    ImGui::Text("Main Features:");
-    ImGui::Indent(15);
-    ImGui::BulletText("Naming convention checking");
-    ImGui::BulletText("Creating color coding");
-    ImGui::BulletText("More advanced query editor");
-    ImGui::BulletText("Sort original files");
-    ImGui::BulletText("Toolbox with helpers:");
-    ImGui::Indent(15);
-    ImGui::BulletText("Delete invalid events");
-    ImGui::BulletText("Reset Fader");
-    ImGui::Unindent(45);
-    ImGui::PopStyleColor();
-    ImGui::Separator();
-
-    ImGui::BulletText("Dig into ImGui and create proper UI setup");
-    ImGui::BulletText("Create Nodebase queryeditor for combining queries");
-    ImGui::BulletText("Sort Originals P4 integration");
-    ImGui::BulletText("QueryEditor: open and import wav file to reaper shortcut select file and ctrl+r");
-    ImGui::BulletText("Remove Unreferenced Sounds");
-    ImGui::BulletText("Remove unused Game syncs");
-    ImGui::BulletText("Analyse audio spectrum of whole project");
-
     ImGui::End();
 }
